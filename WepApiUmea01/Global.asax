954dc116238e95b8afef858277996e4f6a94e397
Peter Benko <Peter Benko@DESKTOP-6L6GM63>
2020-03-20 14:09:30 +0000

    Add project files.

---
+++ WepApiUmea01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WepApiUmea01.MvcApplication" Language="C#" %>
---
+++ WepApiUmea01/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WepApiUmea01.Startup))]
+namespace WepApiUmea01
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
